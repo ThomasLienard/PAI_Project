2dff5656ab282ed96d4b905fb13b3785dc3d8af2
ThomasLienard <thomas.lienard2@gmail.com>
2025-02-19 16:28:34 +0100

    init

---
+++ projet_pai/src/test/java/com/example/projet_pai/ProjetPaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_pai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetPaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
