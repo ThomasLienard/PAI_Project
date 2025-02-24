40b87e0ec4c55be45218b76b1562ca1f1eee49a3
EvannLietard <evann59167@gmail.com>
2025-02-24 16:50:25 +0100

    mongodb up

--- projet_pai/backend/src/main/java/com/example/projet_pai/ProjetPaiApplication.java
+++ projet_pai/backend/src/main/java/com/example/projet_pai/ProjetPaiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.projet_pai.security")
 public class ProjetPaiApplication {
 
 	public static void main(String[] args) {
