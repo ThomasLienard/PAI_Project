188109c4b4bf79f05bbb51a2a2b66044b6b11ac7
Seïf-Eddin Bouguerouche <73120756+Jogozan@users.noreply.github.com>
2025-03-07 13:50:44 +0100

    Front (#1)
    * formulaire de connexion
    
    * change connect to login
    
    * ajout route back
    
    * register
    
    ---------
    
    Co-authored-by: CyrilGodet <cyril.godet.etu@univ-lille.fr>

---
+++ projet_pai/frontend/projet_pai/src/views/ConnectView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="login">
+        <form action="/api/auth/login">
+            <input type="text" placeholder="Nom d'utilisateur">
+            <input type="password" placeholder="Mot de passe">
+            <button type="submit">Se connecter</button>
+        </form>
+    </div>
+</template>
+
+
+<style>
+
+</style>
---
+++ projet_pai/frontend/projet_pai/src/views/RegisterView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="register">
+        <form action="/api/auth/register">
+            <input type="text" placeholder="Nom d'utilisateur">
+            <input type="password" placeholder="Mot de passe">
+            <input type="password" placeholder="Confirmation de mot de passe">
+            <button type="submit">Créer son compte</button>
+        </form>
+    </div>
+</template>
+
+
+<style>
+
+</style>
+<script setup lang="ts">
+</script>
